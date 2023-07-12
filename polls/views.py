7493e7fe0f0300aad3bb5938c093861d7d3dfa43
shakir127 <muhammadshakir56@gmail.com>
2023-07-12 12:29:01 +0500

    #AddChoice

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,69 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Question, Choice
+from django.http import Http404
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+def addchoice(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        New_Choice = request.POST["newchoice"]
+        if(not New_Choice):
+            raise KeyError
+    except (KeyError):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/newchoice.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+                            )
+    else:
+        New_Choice= Choice(
+                    question=question,
+                                    choice_text=New_Choice
+                                    )
+        New_Choice.save()
+        return HttpResponseRedirect(reverse("polls:detail", args=(question.id,)))
